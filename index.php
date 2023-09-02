c11a9b44f7372d243b1231e134964f2967be884d
Ashvinibodade <90124828+Ashvinibodade@users.noreply.github.com>
2023-09-02 14:47:07 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,30 +6,39 @@
         $email=$_POST['email'];
         $sub=$_POST['subject'];
         $mess=$_POST['message'];
+   
+
+        $host="localhost";
+        $username="root";
+        $password="1234";
+        $dbname="contact_form";
+
+        $con=mysqli_connect($host,$username,$password,$dbname);
+
+        if (!$con)
+        {
+            die("Connection failed".mysqli_connect_error());
+        }
+        else
+        {
+            echo " Connection was successfull";
+        }
+
+        $sql="INSERT INTO `contact_form` (`fullname`, `phonenumber`, `Email`, `Subject`, `Message`) VALUES ('$fname', '$pnumber', '$email', '$sub', '$mess')";
+        ";
+
+        $res=mysqli_query($con,$sql);
+
+        if ($res)
+        {
+            echo "Inserted successfully";
+        }
+        else
+        {
+            echo "Record has not been inserted succesfully".mysqli_error($con);
+        
+        }
+
+        mysqli_close($con);
     }
-
-    $host="localhost";
-    $username="root";
-    $password="1234";
-    $dbname="contact_form";
-
-    $con=mysql_connect($host,$username,$password,$dbname);
-
-    if (!$con)
-    {
-        die("Connection failed".mysql_connect_error());
-    }
-    
-
-    $sql="INSERT INTO contact_form (id ,fullname, phonenumber,Email,Subject,Message) VALUES('0','$fname','$pnumber','$email','$sub','$mess')";
-
-    $res=mysql_query($con,$sql);
-
-    if ($res)
-    {
-        echo "Inserted successfully";
-    }
-
-    mysql_close($con);
-
 ?>
