5571e5ead89f9fb75b1158b4358d64c4656e96e3
Ashvinibodade <90124828+Ashvinibodade@users.noreply.github.com>
2023-09-02 14:48:03 +0530

    Update sql.sql

--- sql.sql
+++ sql.sql
@@ -1,10 +1,11 @@
-CREATE SCHEMA `contact_form` ;
+CREATE SCHEMA `contact_form`;
 
 CREATE TABLE `contact_form`.`contact_form` (
-  `id` INT NOT NULL,
-  `fullname` VARCHAR(20) NOT NULL,
-  `phonenumber` INT NOT NULL,
-  `Email` VARCHAR(20) NOT NULL,
-  `Subject` VARCHAR(20) NOT NULL,
-  `Message` VARCHAR(50) NOT NULL,
-  PRIMARY KEY (`idnew_table`));
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `fullname` VARCHAR(50) NOT NULL,
+  `phonenumber` VARCHAR(20) NOT NULL,
+  `Email` VARCHAR(100) NOT NULL,
+  `Subject` VARCHAR(200) NOT NULL,
+  `Message` TEXT NOT NULL,
+  PRIMARY KEY (`id`)
+);
